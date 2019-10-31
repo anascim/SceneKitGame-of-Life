d086cb26647feb1d2853bdac896b9ffbd2827017
anascim <alexjnascim@gmail.com>
2019-10-31 14:30:26 -0300

    Initial commit

---
+++ Game of Life/Game of Life/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Game of Life
+//
+//  Created by Alex Nascimento on 31/10/19.
+//  Copyright © 2019 Alex Nascimento. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
